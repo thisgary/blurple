d7e61f2dd5ef82ae0c8eadcdb5a6ec89798358d9
thisgary <gary.github@gmail.com>
2021-07-22 01:04:44 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+        name='Descord2',
+        packages=find_packages(include=['descord2',]),
+        version='1.0',
+        description='Discord API Python wrapper for learning purposes.',
+        author='thisgary',
+        license='MIT',
+        install_requires=['requests'],
+        setup_requires=['pytest_runner'],
+        test_require=['pytest'],
+        test_suite='tests')
+
