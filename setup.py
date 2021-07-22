c9bdf192ba3baa8a09c3edd487f84f21e34fd609
thisgary <gary.github@gmail.com>
2021-07-22 21:02:14 +0800

    Stable 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
         name='Descord2',
         packages=find_packages(include=['descord2',]),
-        version='0.0-4-gecc410c',
+        version='0.0-5-g73b4c82',
         description='Discord API Python wrapper for learning purposes.',
         author='thisgary',
         license='MIT',
