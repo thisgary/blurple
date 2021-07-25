41ccfbae08fb1fe9e8ba8be476d081520deef6a0
thisgary <gary.github@gmail.com>
2021-07-25 21:44:20 +0800

    Bugfix 2

--- descord/client.py
+++ descord/client.py
@@ -6,6 +6,7 @@ import websockets
 from descord import payload
 
 uri = 'wss://gateway.discord.gg/?v=9&encoding=json'
+cli = {}
 
 async def gateway_heartbeat(intv, ws):
     while True:
@@ -21,16 +22,15 @@ async def gateway_monitor(ws, hb, token):
             await gateway_connect(token, True)
         elif event['op'] is 11: continue
         if 's' in event: 
-            client = client.load(open('client.json'))
-            client['seq'] = event['s']
-            json.dump(client, open('client.json', 'w'))
+            cli = json.load(open('client.json'))
+            cli['seq'] = event['s']
+            json.dump(cli, open('client.json', 'w'))
         print(event)
         open('log.txt', 'a+').write(f'{event}\n')
 
 # Establish websocket connection with Gateway API
 # Alternatively, resume disconnected session
 async def gateway_connect(token, resume=False):
-    client = {}
     async with websockets.connect(uri) as ws:
         hello = await ws.recv()
         hb_intv = payload.data(hello, 'heartbeat_interval')
@@ -38,14 +38,14 @@ async def gateway_connect(token, resume=False):
                 args=(gateway_heartbeat(hb_intv, ws), ))
         hb.start()
         if resume:
-            client = json.load(open('client.json'))
-            session_id, seq = client['session_id'], client['seq']
+            cli = json.load(open('client.json'))
+            session_id, seq = cli['session_id'], cli['seq']
             await ws.send(payload.resume(token, session_id, seq))
         else:
             await ws.send(payload.identify(token))
             ready = await ws.recv()
-            client['session_id'] = payload.data(ready, 'session_id')
-            json.dump(client, open('client.json', 'w'))
+            cli['session_id'] = payload.data(ready, 'session_id')
+            json.dump(cli, open('client.json', 'w'))
         await gateway_monitor(ws, hb, token)
 
 def connect(token):
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
         name='Descord2',
         packages=find_packages(include=['descord',]),
-        version='0.2-2-g01136d4',
+        version='0.2-3-g167e764',
         description='Discord API Python wrapper for learning purposes.',
         author='thisgary',
         license='MIT',
