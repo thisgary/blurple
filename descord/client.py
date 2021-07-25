32d3271cf4cf597090e46a52dc3a3571da4cb0c7
thisgary <gary.github@gmail.com>
2021-07-25 22:01:31 +0800

    Bugfix 3

--- descord/client.py
+++ descord/client.py
@@ -6,7 +6,10 @@ import websockets
 from descord import payload
 
 uri = 'wss://gateway.discord.gg/?v=9&encoding=json'
-cli = {}
+ss = {
+        'session_id': None
+        'seq': None
+        }
 
 async def gateway_heartbeat(intv, ws):
     while True:
@@ -22,9 +25,9 @@ async def gateway_monitor(ws, hb, token):
             await gateway_connect(token, True)
         elif event['op'] is 11: continue
         if 's' in event: 
-            cli = json.load(open('client.json'))
-            cli['seq'] = event['s']
-            json.dump(cli, open('client.json', 'w'))
+            ss = json.load(open('session.json'))
+            ss['seq'] = event['s']
+            json.dump(ss, open('session.json', 'w'))
         print(event)
         open('log.txt', 'a+').write(f'{event}\n')
 
@@ -35,17 +38,17 @@ async def gateway_connect(token, resume=False):
         hello = await ws.recv()
         hb_intv = payload.data(hello, 'heartbeat_interval')
         hb = threading.Thread(target=asyncio.run,
-                args=(gateway_heartbeat(hb_intv, ws), ))
+                args=(gateway_heartbeat(hb_intv, ws),))
         hb.start()
         if resume:
-            cli = json.load(open('client.json'))
-            session_id, seq = cli['session_id'], cli['seq']
+            ss = json.load(open('client.json'))
+            session_id, seq = ss['session_id'], ss['seq']
             await ws.send(payload.resume(token, session_id, seq))
         else:
             await ws.send(payload.identify(token))
             ready = await ws.recv()
-            cli['session_id'] = payload.data(ready, 'session_id')
-            json.dump(cli, open('client.json', 'w'))
+            ss['session_id'] = payload.data(ready, 'session_id')
+            json.dump(ss, open('session.json', 'w'))
         await gateway_monitor(ws, hb, token)
 
 def connect(token):
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
         name='Descord2',
         packages=find_packages(include=['descord',]),
-        version='0.2-3-g167e764',
+        version='0.2-4-g13a5ddc',
         description='Discord API Python wrapper for learning purposes.',
         author='thisgary',
         license='MIT',
