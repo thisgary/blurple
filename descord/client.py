1d9abdbaaa69896d50dcfc4897eb6f23225efc4d
thisgary <gary.github@gmail.com>
2021-07-26 17:10:51 +0800

    Cleanup

--- descord/client.py
+++ descord/client.py
@@ -6,44 +6,137 @@ import websockets
 from descord import payload
 
 
-class Heartbeat:
-    def __init__(self, interval, connection):
-        self.alive = True
-        self.interval = interval/1000
-        self.connection = connection
+class Request:
+    class Path:
+        usr = '/users'
+        gld = '/guilds'
+        chn = '/channels'
+        msg = '/messages'
 
-    async def beat(self):
-        while self.alive:
-            await asyncio.sleep(self.interval)
-            await self.connection.send(payload.heartbeat())
-        print('Stopped heartbeating')
+        def user(self, user_id):
+            return self.url + self.usr + '/' + str(user_id)
 
-    def start(self):
-        threading.Thread(target=asyncio.run, args=(self.beat(),)).start()
+        def me(self):
+            return self.url + '/@me'
 
-    def stop(self):
-        self.alive = False
+        def dms(self):
+            return self.me() + self.chn
+
+        def guilds(self):
+            return self.me() + self.gld
+
+        def guild(self, guild_id):
+            return self.url + self.gld + '/' + str(guild_id)
+
+        def channels(self, guild_id):
+            return self.guild(guild_id) + self.chn
+
+        def channel(self, channel_id):
+            return self.url + self.chn + '/' + str(channel_id)
+
+        def messages(self, channel_id):
+            return self.channel(channel_id) + self.msg
+
+        def message(self, channel_id, message_id):
+            return self.messages(channel_id) + '/' + str(message_id)
+
+
+    def __init__(self, access_token, *, is_bot=True, api_version=9):
+        auth_prefix = 'Bot' if is_bot else 'Bearer'
+        self.auth = {'Authorization': f'{auth_prefix} {access_token.strip()}'}
+        self.url = f'https://discord.com/api/v{api_version}'
+        self.p = self.Path()
+
+    def get(self, path, obj=None):
+        return requests.get(self.url+path, headers=self.auth, params=obj)
+
+    def get_me(self):
+        return self.get(self.p.me())
+
+    def get_user(self, user_id):
+        return self.get(self.p.user(user_id))
+
+    def get_dms(self):
+        return self.get(self.p.dms())
+
+    def get_guilds(self):
+        return self.get(self.p.guilds())
+
+    def get_guild(self, gld_id):
+        return self.get(self.p.guild(gld_id))
+
+    def get_channels(self, gld_id):
+        return self.get(self.p.channels(gld_id))
+
+    def get_channel(self, chn_id):
+        return self.get(self.p.channel(chn_id))
+
+    def get_messages(self, chn_id, hx_obj=None):
+        return self.get(self.p.messages(chn_id), hx_obj)
+
+    def get_message(self, chn_id, msg_id):
+        return self.get(self.p.message(chn_id, msg_id))
+
+    def post(self, path, obj):
+        return requests.post(path, headers=self.auth, json=obj)
+
+    def create_dm(self, user_id):
+        dm_obj = {'recipient_id': user_id}
+        return self.post(self.p.dms(), dm_obj)
+
+    def create_guild(name, **kwargs):
+        guild_obj = {'name': name}
+        guild_obj.update(kwargs)
+        return self.post(self.p.gld, guild_obj)
+
+    def create_channel(self, gld_id, chn_name, **kwargs):
+        chn_obj = {'name': chn_name}
+        chn_obj.update(kwargs)
+        return self.post(self.p.channels(gld_id), chn_obj)
+
+    def create_message(self, chn_id, msg_con, **kwargs):
+        msg_obj = {'content': msg_con}
+        chn_obj.update(kwargs)
+        return self.post(self.p.messages(chn_id), msg_obj)
 
 
 class Gateway:
+    class Heartbeat:
+        def __init__(self, interval, connection):
+            self.alive = True
+            self.interval = interval/1000
+            self.connection = connection
+
+        async def async_start(self):
+            while self.alive:
+                await asyncio.sleep(self.interval)
+                await self.connection.send(payload.heartbeat())
+            print('Stopped heartbeating')
+
+        def start(self):
+            threading.Thread(target=asyncio.run, args=(self.async_start(),)).start()
+
+        def stop(self):
+            self.alive = False
+
+
     def __init__(self, token, *, version=9):
         self.token = token
-        self.hb = None
         self.uri = f'wss://gateway.discord.gg/?v={version}&encoding=json'
+        self.hb = None
 
-    async def bridge(self, resume=False):
+    async def async_connect(self, resume=False):
         async with websockets.connect(self.uri) as ws:
             hello = await ws.recv() # Hello
             hb_intv = payload.data(hello, 'heartbeat_interval')
-            self.hb = Heartbeat(hb_intv, ws)
+            self.hb = self.Heartbeat(hb_intv, ws)
             self.hb.start()
             if not resume:
                 await ws.send(payload.identify(self.token)) # Identify
                 ready = await ws.recv() # Ready
                 ss = {
                         'session_id': payload.data(ready, 'session_id'),
-                        'seq': None
-                        }
+                        'seq': None}
                 json.dump(ss, open('session.json', 'w'))
             else:
                 ss = json.load(open('session.json'))
@@ -66,5 +159,5 @@ class Gateway:
             open('log.txt', 'a+').write(f'{event}\n')
 
     def connect(self):
-        asyncio.run(self.bridge())
+        asyncio.run(self.async_connect())
 
--- descord/payload.py
+++ descord/payload.py
@@ -1,24 +1,24 @@
 import json
 
-def data(pl, attr): # get attr from payload data
+def data(pl, attr): # get from payload data
     d = json.loads(pl)['d']
     if attr in d: return d[attr]
 
-def create(op: int, **d): # return json
+def create(op, **d):
     pl = {'op': op, 'd': d}
     return json.dumps(pl)
 
 def heartbeat():
     return create(1)
 
-def identify(token: str):
+def identify(token):
     prop = {
             '$os': 'linux', 
             '$browser': 'IE', 
-            '$device': 'Nokia-3310'
-            }
+            '$device': 'Nokia-3310'}
     return create(2, token=token, intents=32509, properties=prop)
 
-def resume(token: str, session_id: str, seq: int):
+def resume(token, session_id, seq):
     return create(6, token=token, session_id=session_id, seq=seq)
+
 
