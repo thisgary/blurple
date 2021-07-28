c6059b4baedffb1e58892c031e816c2d5b21f7f6
thisgary <gary.github@gmail.com>
2021-07-28 18:41:22 +0800

    Bugfix 2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='Descord2',
         packages=['descord'],
-        version='0.3-15-gb593ef4',
+        version='0.3-18-g7be7d40',
         description='Discord API wrapper for learning purpose.',
         author='thisgary',
         license='MIT',
