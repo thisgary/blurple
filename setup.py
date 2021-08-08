9154a3beba120301222f65cd159693088940bf92
thisgary <gary.github@gmail.com>
2021-08-08 13:11:39 +0800

    0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='Dscord 2',
-        version='0.4',
+        version='0.5',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Yet another Discord API wrapper.',
