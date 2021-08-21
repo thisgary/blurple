2d8175d09ec8f4b6b88732f451d2a0be90c883a0
thisgary <gary.github@gmail.com>
2021-08-21 13:56:49 +0800

    Update workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='Dscord 2',
-        version='0.7.12',
+        version='0.7.13',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Yet another Discord API wrapper.',
