c6dadce305cc4b087ab9fed6e54145ffc58a0fe8
thisgary <gary.github@gmail.com>
2021-08-22 12:38:00 +0800

    Bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='Dscord 2',
-        version='0.7.24',
+        version='0.7.25',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Yet another Discord API wrapper.',
