479349b736fe0fba652e837b0c09c52e8e26ffb5
thisgary <gary.github@gmail.com>
2021-08-22 12:10:35 +0800

    Another bad prototype

--- client.py
+++ client.py
@@ -1,38 +1,55 @@
-import asyncio
+import threading
 from typing import Callable, List
 
 import dscord
 
-TOKEN = input("Bot Token: ")
 
-gateway = dscord.Gateway(TOKEN)
-request = dscord.Request(TOKEN)
+class Cli:
+    def __init__(self, access_token: str):
+        self.gate = dscord.Gateway(access_token)
+        self.req = dscord.Request(access_token)
+
+    def guild_list(self):
+        guilds = [(g['name'], g['id']) for g in self.req.get_guilds().json()]
+        s = '\nGUILDS\n'
+        for g in guilds:
+            s += f'{g[0]} - {g[1]} \n'
+        return s
+
+    def listen(self):
+        while True:
+            inp = input()
+            if inp in ['/help', '/h']:
+                print('WIP')
+            elif inp in ['/quit', '/exit', '/q']:
+                print('Exiting..')
+                self.gate.stop()
+                break
+
+    def start(self):
+        print(self.guild_list())
+        threading.Thread(target=self.gate.start).start()
+        print('Enter /help for more info!')
+        self.listen()
 
 
-def guild_list() -> List[dict]:
-    return [d for d in request.get_guilds().json()]
+client = Cli(input('Bot token: '))
 
 
-def scope(header: str, dicts: List[dict]) -> str:
-    s, i = '\n' + header + '\n', 0
-    for d in dicts:
-        s += f"[{i}] {d['name']} ({d['id']}) \n"
-        i += 1
-    return s
-
-
-@gateway.event
-async def message_create(p):
+@client.gate.event
+async def on_message(p):
     if p.t == 'MESSAGE_CREATE':
         content = p.d['content']
         if content == '': return
-        author, timestamp = p.d['author'], p.d['timestamp']
-        g, c = p.d['guild_id'][:3], p.d['channel_id'][:3]
-        t = timestamp.split('T')[1][:8]
+        t = p.d['timestamp'].split('T')[1][:8]
+        gld_id, chn_id = p.d['guild_id'], p.d['channel_id']
+        if gld_id[:3] == chn_id[:3]:
+            g, c = f'..{gld_id[-3:]}', f'..{chn_id[-3:]}'
+        else:
+            g, c = f'{gld_id[:3]}..', f'{chn_id[:3]}..'
+        author = p.d['author']
         a = f"{author['username']}#{author['discriminator']}"
-        print(f'{t} ({g}..,{c}..) {a}: {content}')
+        print(f'{t} ({g}, {c}) {a}: {content}')
 
 
-print(scope("Guilds: ", guild_list()))
-
-gateway.start()
+client.start()
