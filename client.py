724898704836fef5b65d05af1843466a1d5c126b
thisgary <gary.github@gmail.com>
2021-08-23 01:08:53 +0800

    Cleanup

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
+from getpass import getpass
 import threading
 from typing import Callable, List
 
 import dscord
+
+history = []
 
 
 class Cli:
@@ -18,10 +21,22 @@ class Cli:
 
     def listen(self):
         while True:
-            inp = input()
-            if inp in ['/help', '/h']:
-                print('WIP')
-            elif inp in ['/quit', '/exit', '/q']:
+            user_input = input()
+            if user_input[0] != '/': continue
+            cmd, *tail = user_input[1:].split(' ')
+            if cmd in ['help', 'h']:
+                print('[KYS]')
+            elif cmd in ['send', 's']:
+                if tail[0][0] == '-':
+                    flag, arg, *tail = tail
+                    if flag == '-l' and arg.isnumeric():
+                        pos = 0 - (int(arg) + 1)
+                        scope = history[pos]
+                else:
+                    scope = history[-1]
+                msg = vars(dscord.Message(' '.join(tail)))
+                self.req.post_message(scope[1], msg)
+            elif cmd in ['quit', 'exit', 'q']:
                 print('Exiting..')
                 self.gate.stop()
                 break
@@ -33,7 +48,7 @@ class Cli:
         self.listen()
 
 
-client = Cli(input('Bot token: '))
+client = Cli(getpass('Bot token: '))
 
 
 @client.gate.event
@@ -42,14 +57,18 @@ async def on_message(p):
         content = p.d['content']
         if content == '': return
         t = p.d['timestamp'].split('T')[1][:8]
-        gld_id, chn_id = p.d['guild_id'], p.d['channel_id']
-        if gld_id[:3] == chn_id[:3]:
-            g, c = f'..{gld_id[-3:]}', f'..{chn_id[-3:]}'
+        scope = (p.d['guild_id'], p.d['channel_id'])
+        global history
+        if scope in history:
+            history.remove(scope)
+        history.append(scope)
+        if scope[0][:3] == scope[1][:3]:
+            s = f'(..{scope[0][-3:]}, ..{scope[1][-3:]})'
         else:
-            g, c = f'{gld_id[:3]}..', f'{chn_id[:3]}..'
+            s = f'({scope[0][:3]}.., {scope[1][:3]}..)'
         author = p.d['author']
         a = f"{author['username']}#{author['discriminator']}"
-        print(f'{t} ({g}, {c}) {a}: {content}')
+        print(f'{t} {s} {a}: {content}')
 
 
 client.start()
